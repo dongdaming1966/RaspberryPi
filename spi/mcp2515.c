9a8fa6620ad78871485760935370deb28bc38c54
dongdaming <dongdaming1994@gmail.com>
2018-03-20 06:16:11 +0000

    update mcp2515.c

--- spi/mcp2515.c
+++ spi/mcp2515.c
@@ -3,11 +3,27 @@
 #include	<unistd.h>  
 #include	<sys/ioctl.h>
 #include	<stdint.h>  
-#include	<linux/spi/spidev.h> 
+#include	<linux/spi/spidev.h>
+#include	<stdarg.h>		//Variable Arguments
+
 #define 	SPI_PATH "/dev/spidev0.0"
 #define 	dataconvert(x)	(x&0x2000)? x|0xc000:x&0x1fff 
 
-uint8_t	received_data[12];
+#define		INST_RESET 0xC0
+#define		INST_READ 0x03
+#define		INST_RXBUFF 0x90
+#define		INST_WRITE 0x02
+#define		INST_TXBUFF 0x40
+
+#define		ADDR_TXB0CTRL 0x30
+#define		ADDR_TXB0SIDH 0x31
+#define		ADDR_TXB0SIDL 0x32
+#define		ADDR_TXB0DLC 0x35
+#define		ADDR_TXB0D0 0x36
+
+#define		SET_TXBUFF(num,id,len) 6,INST_TXBUFF+num*2,(id&0x7f8)>>3,(id&0x07)<<5,0x00,0x00,len
+#define		SET_TXDATA(num,len) len+1,INST_TXBUFF+1+num*2
+#define		SET_TXSEND(num) 3,INST_WRITE,ADDR_TXB0CTRL+num*16,0x0b
 
 int spi_init(void)
 {
@@ -58,43 +74,92 @@ int transfer(int fd, uint8_t* send, uint8_t* receive,uint8_t len)
 
 } 
 
-uint8_t mcp_transfer(int fd, uint8_t instruction, uint8_t addr, uint8_t data)
+uint8_t mcp_transfer(int fd, uint8_t len,...)
 {
-	uint8_t send[9]={0x03,0x30,0x7b};
-	uint8_t receive[9];
+	va_list valist;
+	uint8_t send[99];
+	uint8_t receive[99];
 	int i;
-	send[0]=instruction;
-	send[1]=addr;
-	send[2]=data;
-	
-	transfer(fd,send,receive,3);	
 
-	return receive[2];
+	va_start(valist,len);
+
+//	printf("send: ");
+	for(i=0;i<len;i++)
+//	{
+		send[i]=va_arg(valist,int);
+//		printf("%x ",send[i]);
+//	}
+//	printf("\n");
+
+	transfer(fd,send,receive,len);
+
+//	printf("get: %x\n",receive[len-1]);
+
+	return receive[len-1];
 }
 
+int mcp_init(void)
+{
+	int fd;
+
+	fd=spi_init();
+
+	mcp_transfer(fd,3,INST_WRITE,0x3f,0x87);	//CANSTAT:Configuration mode
+	mcp_transfer(fd,3,INST_WRITE,0x2a,0x00);	//CNF1
+	mcp_transfer(fd,3,INST_WRITE,0x29,0x82);	//CNF2
+	mcp_transfer(fd,3,INST_WRITE,0x28,0x02);	//CNF3
+
+	mcp_transfer(fd,3,INST_WRITE,0x3f,0x07);	//CANSTAT
+
+	return fd;
+}
+int mcp_print(int fd, int addr, int len)
+{
+	int rev,i;
+
+	for(i=0;i<len;i++)
+	{
+		rev=mcp_transfer(fd,3,INST_READ,addr+i,0x00);
+		printf("%x = %x\n",addr+i,rev);
+	}
+
+	return 0;
+}
+
+int mcp_setdata(int fd,int addr,int len,...)
+{
+	va_list valist;
+	int i,p;
+
+	va_start(valist,len);
+
+	mcp_transfer(fd,3,INST_WRITE,ADDR_TXB0SIDH,0x01);	//SIDH
+	mcp_transfer(fd,3,INST_WRITE,ADDR_TXB0SIDL,0x40);	//SIDL
+	mcp_transfer(fd,3,INST_WRITE,ADDR_TXB0DLC,0x01);	//DLC
+	mcp_transfer(fd,3,INST_WRITE,ADDR_TXB0D0,0xff);		//D0
+	mcp_transfer(fd,3,INST_WRITE,ADDR_TXB0CTRL,0x0b);	//TXB0CTRL
+
+//	printf("addr:%d\nlen: %d\nprint: ",addr,len);
+	for(i=0;i<len;i++)
+	{
+		p=va_arg(valist,int);
+	//	printf("%d ",p);
+	}
+//	printf("\n");
+	return 0;
+}
 
 int main()
 {
 	int fd,rev;
 	
-	fd=spi_init();
+	fd=mcp_init();	
 
-	mcp_transfer(fd,0x02,0x3f,0x87);	//CANSTAT:Configuration mode
-	mcp_transfer(fd,0x02,0x2a,0x00);	//CNF1
-	mcp_transfer(fd,0x02,0x29,0x82);	//CNF2
-	mcp_transfer(fd,0x02,0x28,0x02);	//CNF3
-	mcp_transfer(fd,0x02,0x31,0x01);	//SIDH
-	mcp_transfer(fd,0x02,0x32,0x40);	//SIDL
-	mcp_transfer(fd,0x02,0x35,0x01);	//DLC
-	mcp_transfer(fd,0x02,0x36,0xff);	//D0
-	mcp_transfer(fd,0x02,0x30,0x0b);	//TXB0CTRL
-	mcp_transfer(fd,0x02,0x3f,0x07);	//CANSTAT
+	mcp_transfer(fd,SET_TXBUFF(0,0x301,5));
+	mcp_transfer(fd,SET_TXDATA(0,5),0x0f,0x00,0x00,0x00,0x00);
+	mcp_transfer(fd,SET_TXSEND(0));
 
-
-	rev=mcp_transfer(fd,0x03,0x29,0x00);
-	
-
-	printf("rev = %x\n",rev);
+	mcp_print(fd,0x30,16);
 
 	return 0;
 }
